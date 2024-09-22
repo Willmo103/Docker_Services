a3fa1858e8e97359d73b6b69dc7f0af14513501e
Will Morris <willmorris103@gmail.com>
2024-09-21 21:17:44 -0500

    updated entrypoint: Git_Clone_and_Flatten

--- Git_Clone_and_Flatten/entrypoint.sh
+++ Git_Clone_and_Flatten/entrypoint.sh
@@ -9,23 +9,24 @@ fi
 # Clone the repository
 git clone $1 /tmp/repo
 
+# Get the repository name
+REPO_NAME=$(basename -s .git $(echo $1 | sed 's/\/$//'))
+
 # Set up compression flag
 COMPRESS_FLAG=""
 if [ "$COMPRESS" = "true" ]; then
     COMPRESS_FLAG="-c"
 fi
 
-ls /app
-
 # Make sure CodeFlattener is executable
 chmod +x /app/CodeFlattener
 
 # Run the CodeFlattener
-/app/CodeFlattener /tmp/repo /output/output.md $COMPRESS_FLAG
+/app/CodeFlattener /tmp/repo /output/${REPO_NAME}.md $COMPRESS_FLAG
 
 # Check if the output file was created
-if [ -f /output/output.md ]; then
-    echo "Repository flattened successfully. Output saved to /output/output.md"
+if [ -f /output/${REPO_NAME}.md ]; then
+    echo "Repository flattened successfully. Output saved to /output/${REPO_NAME}.md"
     if [ "$COMPRESS" = "true" ]; then
         echo "Compression was applied."
     fi
